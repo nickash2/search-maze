7298ee1d4368c4ab43bf8eddac78c3c92567978d
nickash2 <nickash015@gmail.com>
2023-04-25 15:20:14 +0200

    Implemented fundamentals for IDS

--- labfiles/maze_solver.py
+++ labfiles/maze_solver.py
@@ -4,7 +4,6 @@ from state import State
 
 
 def solve_maze(maze, fr):
-    
     # get the start room, create state with start room and None as parent and put it in fringe
     room = maze.get_room(*maze.get_start())
     state = State(room, None)
@@ -48,6 +47,60 @@ def solve_maze(maze, fr):
     return False
 
 
+def solveIDS(maze, fr):
+    room = maze.get_room(*maze.get_start())
+    state = State(room, None, depth=0)
+    
+    depth_limit = 0
+    
+    fr.push(state)
+    visited = set()    # set consisting of only the visited rooms
+
+    while not fr.is_empty():
+        if ((state.get_depth() <= depth_limit)):
+            # get item from fringe and get the room from that state
+            state = fr.pop()
+            room = state.get_room()
+
+            if room.is_goal():
+                # if room is the goal, print that with the statistics and the path and return
+                print("solved")
+                fr.print_stats()
+                state.print_path()
+                state.print_actions()
+                print()  # print newline
+                maze.print_maze_with_path(state)
+                return True
+
+            if room not in visited:
+                # if room was not visited before, add it to the set
+                visited.add(room)
+                for d in room.get_connections():    # for move in [N,S,E,W]
+                    # loop through every possible move
+                    # Get new room after move and cost to get there
+                    new_room, cost = room.make_move(d, state.get_cost())
+                    if new_room not in visited:
+                        # create the new state and push to the stack if the new room has not been visited before
+                        # Create new state with new room and old room
+                        new_state = State(new_room, state, cost, depth = state.get_depth() + 1)
+                        # push the new state
+                        fr.push(new_state)
+        else:
+            depth_limit += 1
+            continue
+        
+            
+        
+
+    # fringe is empty and goal is not found, so maze is not solved
+    print("not solved")
+    fr.print_stats()        # print the statistics of the fringe
+    return False
+    
+
+    
+
+
 def solve_maze_general(maze, algorithm):
     """
     Finds a path in a given maze with the given algorithm
@@ -68,7 +121,7 @@ def solve_maze_general(maze, algorithm):
         fr = GreedyFringe("PRIORITY")
     elif algorithm == "IDS":
         fr = Fringe("STACK")
-        if (IDSAlgorithm(maze, fr)):
+        if (solveIDS(maze, fr)):
             return True
         return False
     else:
@@ -80,6 +133,5 @@ def solve_maze_general(maze, algorithm):
     return False
     
 
-def IDSAlgorithm(maze, fr):
-    pass
+
 
--- labfiles/state.py
+++ labfiles/state.py
@@ -4,11 +4,19 @@ import maze
 
 class State:
 	"""Class to save the possible states in"""
-	def __init__(self, room, parent, cost=0, priority=0):
+	def __init__(self, room, parent, cost=0, priority=0, depth=0):
 		self.parent = parent
 		self.room = room
 		self.cost = cost
 		self.priority = priority
+		self.depth = depth
+  
+	def get_depth(self):
+		return self.depth
+
+	def set_depth(self, depth):
+		self.depth = depth
+
 
 	def get_room(self):
 		"""
