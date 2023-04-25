4a988653587609f86478e85ab7f0474f85d8802b
nickash2 <nickash015@gmail.com>
2023-04-25 18:58:51 +0200

    Implemented basic IDS functionality

--- labfiles/maze_solver.py
+++ labfiles/maze_solver.py
@@ -50,57 +50,55 @@ def solve_maze(maze, fr):
 def solveIDS(maze, fr):
     room = maze.get_room(*maze.get_start())
     state = State(room, None, depth=0)
-    
+    fr.push(state)
+    visited = set()
     depth_limit = 0
     
-    fr.push(state)
-    visited = set()    # set consisting of only the visited rooms
-
-    while not fr.is_empty():
-        if ((state.get_depth() <= depth_limit)):
-            # get item from fringe and get the room from that state
-            state = fr.pop()
-            room = state.get_room()
-
-            if room.is_goal():
-                # if room is the goal, print that with the statistics and the path and return
-                print("solved")
-                fr.print_stats()
-                state.print_path()
-                state.print_actions()
-                print()  # print newline
-                maze.print_maze_with_path(state)
+    while True:
+        if not fr.is_empty():
+            if(DFS(state, visited, fr, depth_limit, maze)):
+            # goal found
                 return True
-
-            if room not in visited:
-                # if room was not visited before, add it to the set
-                visited.add(room)
-                for d in room.get_connections():    # for move in [N,S,E,W]
-                    # loop through every possible move
-                    # Get new room after move and cost to get there
-                    new_room, cost = room.make_move(d, state.get_cost())
-                    if new_room not in visited:
-                        # create the new state and push to the stack if the new room has not been visited before
-                        # Create new state with new room and old room
-                        new_state = State(new_room, state, cost, depth = state.get_depth() + 1)
-                        # push the new state
-                        fr.push(new_state)
-        else:
             depth_limit += 1
-            continue
-        
-            
-        
+    
+    
+def DFS(state, visited, fr, depth_limit, maze):
+    
+    # get item from fringe and get the room from that state
+    state = fr.pop()
+    room = state.get_room()
 
-    # fringe is empty and goal is not found, so maze is not solved
-    print("not solved")
-    fr.print_stats()        # print the statistics of the fringe
+    if room.is_goal(): 
+        print("solved")
+        fr.print_stats()
+        state.print_path()
+        state.print_actions()
+        print()
+        maze.print_maze_with_path(state)
+        return True
+    
+    if state.get_depth() > depth_limit:
+        print("depth limit reached")
+        return False
+
+    if room not in visited:
+        # if room was not visited before, add it to the set
+        visited.add(room)
+        for d in room.get_connections():    # for move in [N,S,E,W]
+            # loop through every possible move
+            # Get new room after move and cost to get there
+            new_room, cost = room.make_move(d, state.get_cost())
+            if new_room not in visited:
+                # create the new state and push to the stack if the new room has not been visited before
+                # Create new state with new room and old room
+                new_state = State(new_room, state, cost, depth = state.get_depth() + 1)
+                # push the new state
+                fr.push(new_state)
+
+    # goal is not found, so maze is not solved
     return False
     
-
     
-
-
 def solve_maze_general(maze, algorithm):
     """
     Finds a path in a given maze with the given algorithm
@@ -131,7 +129,9 @@ def solve_maze_general(maze, algorithm):
     if (solve_maze(maze,fr)):
         return True
     return False
-    
+
+
+
 
 
 
